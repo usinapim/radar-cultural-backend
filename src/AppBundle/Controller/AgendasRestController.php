09205f30ba281c133a187a61577cef3a810137af
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2016-04-20 09:34:50 -0300

    #4

--- src/AppBundle/Controller/AgendasRestController.php
+++ src/AppBundle/Controller/AgendasRestController.php
@@ -2,23 +2,50 @@
 
 namespace AppBundle\Controller;
 
-//use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
 use FOS\RestBundle\Controller\FOSRestController;
 
 class AgendasRestController extends FOSRestController {
 
-	public function getAgendasAction(  ) {
-		$em = $this->getDoctrine()->getManager();
+	public function getAgendasAction() {
+		$em      = $this->getDoctrine()->getManager();
 		$agendas = $em->getRepository( 'AppBundle:Agenda' )->findAll();
 
-		$vista = $this->view( $agendas,
+		$aAgendas = array();
+
+		foreach ( $agendas as $agenda ) {
+
+			$texto = $agenda->getFechaEventoDesde()->format( 'd-m-Y' );
+			$index = $this->findGrupoIndex( $aAgendas, $texto );
+			if ( $index !== false ) {
+				//es un grupo
+				$aAgendas[ $index ]['eventos'][] = $agenda;
+			} else {
+				//creo otro grupo
+				$index = count( $aAgendas );
+
+				$aAgendas[ $index ]['texto']     = $texto;
+				$aAgendas[ $index ]['eventos'][] = $agenda;
+			}
+
+
+		}
+
+		$vista = $this->view( $aAgendas,
 			200 )
-//			->setTemplate( "MyBundle:Users:getUsers.html.twig" )
-//			->setTemplateVar( 'noticias' )
-		;
+		              ->setTemplate( "AppBundle:Rest:getAgendas.html.twig" )
+		              ->setTemplateVar( 'agendas' );
 
 		return $this->handleView( $vista );
+	}
+
+	private function findGrupoIndex( $array, $texto ) {
+		foreach ( $array as $index => $item ) {
+			if ( $item['texto'] == $texto ) {
+				return $index;
+			}
+		}
+
+		return false;
 	}
 
 }
